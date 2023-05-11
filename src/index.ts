99b546b0997f6efa1f85078f428a5be507b7d191
shukurullox0507 <shukurullox0507@gmail.com>
2023-05-11 14:11:39 +0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+
+
+const box = document.querySelector('.box');
+const loader = document.querySelector('.spinners');
+const updateBtn = document.querySelector('.update-picture');
+const images = document.querySelectorAll('img');
+loader!.classList.remove('vanish');
+
+
+let imagesLoaded = 0;
+
+images.forEach((img) => {
+    img.classList.add('vanish')
+    img.addEventListener('load', () => {
+        imagesLoaded++;
+        if (imagesLoaded == 4) {
+            images.forEach((img) => {
+                loader!.classList.add('vanish');
+                img.classList.remove('vanish')
+            });
+
+        }
+
+
+
+    })
+})
+updateBtn?.addEventListener("click", () =>{
+    location.reload()
+})
